d5f194f71f2e2ccc9105bbf6339800c72d1d17c5
Zane Wang <zelinwan@andrew.cmu.edu>
2023-10-25 14:25:21 -0400

    finished lab08: Reimplement the Promise version of  as an async version (in )

--- src/index.ts
+++ src/index.ts
@@ -38,8 +38,38 @@ main([
 
 // Implement the async version of the above here
 // Your version should not use .then and should use try/catch instead of .catch
+
+// In mainAsync(fileNames), 
+// We loop over the fileNames with a for...of loop.
+// Within the loop, we try to get the logo detection results using the await keyword.
+// If the detection is successful, we process the results and print them.
+// If an error occurs, we catch it in the catch block and handle it accordingly.
 async function mainAsync(fileNames: string[]): Promise<void> {
-    // Your code here
+    for (const fileName of fileNames) {
+        console.log(`Running logo detection on ${fileName}`);
+        
+        try {
+            const [result] = await client.logoDetection(fileName);
+            let scores: number[] = [];
+            const logos = result.logoAnnotations;
+            logos?.forEach((logo) => {
+                if (logo.description)
+                    console.log(`"${logo.description}" found in in file ${fileName}`);
+                if (logo.score)
+                    scores.push(logo.score);
+            });
+            const avg = scores.reduce((a, b) => a + b, 0) / scores.length;  // Added 0 as initial value for reduce
+            console.log(`Average score for ${fileName}: ${avg}`);
+        } catch (err: unknown) {  // Explicitly set err type as unknown, added explicit checks on the err object inside the catch block
+            if (typeof err === 'object' && err !== null && 'code' in err && err.code === 'ENOENT') {
+                console.log(`File ${fileName} not found`);
+            } else if (typeof err === 'object' && err !== null && 'message' in err) {
+                console.error(`Error processing ${fileName}: ${(err as {message: string}).message}`);
+            } else {
+                console.error(`An unknown error occurred processing ${fileName}`);
+            }
+        }
+    }
 }
 
 mainAsync([
